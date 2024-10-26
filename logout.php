a9f308a565d9fe082d603216163c865239e633dc
HAFASHIMANALEONIDAS <hafashimanaleonidas8@gmail.com>
2024-10-26 15:52:03 +0200

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,19 +1,6 @@
 <?php
-// Start the session to manage session variables
 session_start();
-
-// Destroy the session to log the user out
 session_destroy();
-
-// Clear any cookies set for the user
-if (isset($_COOKIE['email'])) {
-    setcookie('email', '', time() - 3600); // Expire the cookie by setting a past time
-}
-if (isset($_COOKIE['pass'])) {
-    setcookie('pass', '', time() - 3600); // Expire the cookie by setting a past time
-}
-
-// Provide a logout confirmation message with a link to log back in
-echo "<h1>You have successfully logged out.</h1>";
-echo "<p>Click here to <a href='login.php'>login again</a>.</p>";
+echo"You successfull logout.click here <a href='login.php'>
+login </a>";
 ?>
--- welcome.php
+++ welcome.php
@@ -1,17 +1,5 @@
-
 <?php
-// Start the session to access session variables
 session_start();
-
-// Check if the user is logged in
-if (isset($_SESSION['email'])) {
-    // Display a welcome message to the user
-    echo "<h1>Welcome, " . htmlspecialchars($_SESSION['email']) . "!</h1>";
-    // Provide a logout link
-    echo "<p><a href='logout.php'>Logout</a></p>";
-} else {
-    // If the user is not logged in, redirect to the login page
-    header("Location: login.php");
-    exit; // Ensure script termination after redirection
-}
+echo"Welcome". $_SESSION['email'];
+echo"<a href='logout.php'>logout</a>";
 ?>
